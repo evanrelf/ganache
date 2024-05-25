318fce5f26b822d21a76be460329ca3e4fe6635c
Evan Relf <evan@evanrelf.com>
2024-05-25 11:20:12 -0700

    Move code for executable out of library
    In theory you should be able to upload the library to Hackage and have
    it make sense, even if I don't plan to publish it there.

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,21 @@
 module Main (main) where
 
-import Ganache qualified
+import Options.Applicative qualified as Options
+
+data Options = Options {}
+
+parseOptions :: Options.Parser Options
+parseOptions = do
+  pure Options{}
+
+getOptions :: IO Options
+getOptions = do
+  let parserPrefs = Options.prefs mempty
+  let parserInfo =
+        Options.info (Options.helper <*> parseOptions) mempty
+  Options.customExecParser parserPrefs parserInfo
 
 main :: IO ()
-main = Ganache.main
+main = do
+  _options <- getOptions
+  putStrLn "Hello world!"
