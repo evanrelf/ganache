76079fabcb133479b4ca0e3cdf4c6c8d88bfc593
Evan Relf <evan@evanrelf.com>
2024-05-31 00:01:18 -0700

    Copy example ACH files from `moov-io/ach`

--- examples/README.md
+++ examples/README.md
@@ -1,6 +1,5 @@
 Sources:
 
-- [Example 1](https://www.hancockwhitney.com/hubfs/Treasury%20Services%20Resource%20Library%20Files/ACH%20Services/NACHA-Sample-File.txt)
-- [Examples 2-8](https://www.ach-pro.com/post/common-nacha-file-examples)
-
-TODO: Copy examples from `moov-io/ach` [here](https://github.com/moov-io/ach/tree/master/examples/testdata).
+- [`./1.ach`](https://www.hancockwhitney.com/hubfs/Treasury%20Services%20Resource%20Library%20Files/ACH%20Services/NACHA-Sample-File.txt)
+- [`./{2..8}.ach`](https://www.ach-pro.com/post/common-nacha-file-examples)
+- [`./moov/*.ach`](https://github.com/moov-io/ach/tree/master/examples/testdata)
--- test/GanacheTest.hs
+++ test/GanacheTest.hs
@@ -1,36 +1,58 @@
 module GanacheTest (module GanacheTest) where
 
+import Control.Exception.Safe (MonadCatch)
 import Data.ByteString qualified as ByteString
+import Data.List qualified as List
 import FlatParse.Basic qualified as FlatParse
 import Ganache
+import Streamly.Data.Stream (Stream)
 import Streamly.Data.Stream qualified as Stream
-import Streamly.Internal.FileSystem.Dir qualified as Streamly (readFiles)
-import System.FilePath ((</>))
+import Streamly.Internal.FileSystem.Dir qualified as Streamly
 import System.FilePath qualified as FilePath
 import Test.Tasty
+import Test.Tasty.ExpectedFailure (ignoreTest)
 import Test.Tasty.HUnit
 import Text.Megaparsec qualified as Megaparsec
 
 test_roundtripExamples :: IO [TestTree]
 test_roundtripExamples =
-    Streamly.readFiles "examples/"
-  & fmap (\path -> "examples" </> path)
+    readFilesRecursive "examples/"
   & Stream.filter (\path -> "ach" `FilePath.isExtensionOf` path)
   & Stream.mapM (\path -> (path,) <$> ByteString.readFile path)
-  & fmap (\(path, bytes) -> testGroup path
-      [ testCase "flatparse" do
-          achFile <- flatparse @AchFile bytes
-          assertEqual "Prints original file" bytes (toAch achFile)
-      , testCase "megaparsec" do
-          achFile <- megaparsec @AchFile path bytes
-          assertEqual "Prints original file" bytes (toAch achFile)
-      , testCase "flatparse and megaparsec agree" do
-          achFileF <- flatparse @AchFile bytes
-          achFileM <- megaparsec @AchFile path bytes
-          assertEqual "flatparse and megaparsec agree" achFileF achFileM
-      ]
+  & fmap (\(path, bytes) ->
+      ( path
+      , testGroup path
+          [ testCase "flatparse" do
+              achFile <- flatparse @AchFile bytes
+              assertEqual "Prints original file" bytes (toAch achFile)
+          , testCase "megaparsec" do
+              achFile <- megaparsec @AchFile path bytes
+              assertEqual "Prints original file" bytes (toAch achFile)
+          , testCase "flatparse and megaparsec agree" do
+              achFileF <- flatparse @AchFile bytes
+              achFileM <- megaparsec @AchFile path bytes
+              assertEqual "flatparse and megaparsec agree" achFileF achFileM
+          ]
+      )
+    )
+  -- TODO: Fix parsing of `moov` ACH files
+  & fmap (\(path, test) ->
+      ( path
+      , if "moov" `List.isInfixOf` path
+          then ignoreTest test
+          else test
+      )
     )
   & Stream.toList
+  & fmap (sortOn fst >>> fmap snd)
+
+readFilesRecursive :: (MonadIO m, MonadCatch m) => FilePath -> Stream m FilePath
+readFilesRecursive root =
+    Streamly.readEitherPaths root
+  & Stream.concatMap (\case
+      Left dir -> readFilesRecursive dir
+      Right file -> Stream.fromPure file
+    )
 
 flatparse :: forall a. FromAch a => ByteString -> IO a
 flatparse bytes =
