8dc15b06ab950a41f807e49d7e43ada41b2f0c7d
Evan Relf <evan@evanrelf.com>
2024-05-30 21:32:04 -0700

    Break `AchBatchControlRecord` into opaque fields

--- lib/Ganache/Data/AchBatchControlRecord.hs
+++ lib/Ganache/Data/AchBatchControlRecord.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Ganache.Data.AchBatchControlRecord
@@ -14,21 +15,66 @@ import Ganache.Class.ToAch
 import Text.Megaparsec qualified as M
 import Text.Megaparsec.Byte qualified as M
 
-newtype AchBatchControlRecord = AchBatchControlRecord ByteString
+data AchBatchControlRecord = AchBatchControlRecord
+  { recordTypeCode :: !ByteString
+  , serviceClassCode :: !ByteString
+  , entryAndAddendaCount :: !ByteString
+  , entryHash :: !ByteString
+  , totalDebit :: !ByteString
+  , totalCredit :: !ByteString
+  , companyIdentification :: !ByteString
+  , messageAuthenticationCode :: !ByteString
+  , reserved :: !ByteString
+  , originatingDfiIdentification :: !ByteString
+  , batchNumber :: !ByteString
+  }
 
 instance FromAch AchBatchControlRecord where
   parseAchF :: ParserF AchBatchControlRecord
   parseAchF = do
     $(F.char '8')
-    bytes <- F.take 93
-    pure $ AchBatchControlRecord bytes
+    let recordTypeCode = Char8.singleton '8'
+    serviceClassCode <- F.take 3
+    entryAndAddendaCount <- F.take 6
+    entryHash <- F.take 10
+    totalDebit <- F.take 12
+    totalCredit <- F.take 12
+    companyIdentification <- F.take 10
+    messageAuthenticationCode <- F.take 19
+    reserved <- F.take 6
+    originatingDfiIdentification <- F.take 8
+    batchNumber <- F.take 7
+    pure AchBatchControlRecord{..}
 
   parseAchM :: ParserM AchBatchControlRecord
   parseAchM = do
     _ <- M.char (c2w '8')
-    bytes <- M.takeP Nothing 93
-    pure $ AchBatchControlRecord bytes
+    let recordTypeCode = Char8.singleton '8'
+    serviceClassCode <- M.takeP Nothing 3
+    entryAndAddendaCount <- M.takeP Nothing 6
+    entryHash <- M.takeP Nothing 10
+    totalDebit <- M.takeP Nothing 12
+    totalCredit <- M.takeP Nothing 12
+    companyIdentification <- M.takeP Nothing 10
+    messageAuthenticationCode <- M.takeP Nothing 19
+    reserved <- M.takeP Nothing 6
+    originatingDfiIdentification <- M.takeP Nothing 8
+    batchNumber <- M.takeP Nothing 7
+    pure AchBatchControlRecord{..}
 
 instance ToAch AchBatchControlRecord where
   toAch :: AchBatchControlRecord -> ByteString
-  toAch (AchBatchControlRecord bytes) = Char8.cons '8' bytes
+  toAch x =
+    mconcat
+      [ x.recordTypeCode
+      , x.serviceClassCode
+      , x.entryAndAddendaCount
+      , x.entryHash
+      , x.totalDebit
+      , x.totalCredit
+      , x.companyIdentification
+      , x.messageAuthenticationCode
+      , x.reserved
+      , x.originatingDfiIdentification
+      , x.batchNumber
+      ]
