1d820427d2f8533dd8b263b0b5ee488d3802e4a2
Evan Relf <evan@evanrelf.com>
2024-05-30 21:45:11 -0700

    Break `AchFileControlRecord` into opaque fields

--- lib/Ganache/Data/AchFileControlRecord.hs
+++ lib/Ganache/Data/AchFileControlRecord.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Ganache.Data.AchFileControlRecord
@@ -14,21 +15,54 @@ import Ganache.Class.ToAch
 import Text.Megaparsec qualified as M
 import Text.Megaparsec.Byte qualified as M
 
-newtype AchFileControlRecord = AchFileControlRecord ByteString
+data AchFileControlRecord = AchFileControlRecord
+  { recordTypeCode :: !ByteString
+  , batchCount :: !ByteString
+  , blockCount :: !ByteString
+  , entryAndAddendaCount :: !ByteString
+  , entryHash :: !ByteString
+  , totalDebit :: !ByteString
+  , totalCredit :: !ByteString
+  , reserved :: !ByteString
+  }
 
 instance FromAch AchFileControlRecord where
   parseAchF :: ParserF AchFileControlRecord
   parseAchF = do
     $(F.char '9')
-    bytes <- F.take 93
-    pure $ AchFileControlRecord bytes
+    let recordTypeCode = Char8.singleton '9'
+    batchCount <- F.take 6
+    blockCount <- F.take 6
+    entryAndAddendaCount <- F.take 8
+    entryHash <- F.take 10
+    totalDebit <- F.take 12
+    totalCredit <- F.take 12
+    reserved <- F.take 39
+    pure AchFileControlRecord{..}
 
   parseAchM :: ParserM AchFileControlRecord
   parseAchM = do
     _ <- M.char (c2w '9')
-    bytes <- M.takeP Nothing 93
-    pure $ AchFileControlRecord bytes
+    let recordTypeCode = Char8.singleton '9'
+    batchCount <- M.takeP Nothing 6
+    blockCount <- M.takeP Nothing 6
+    entryAndAddendaCount <- M.takeP Nothing 8
+    entryHash <- M.takeP Nothing 10
+    totalDebit <- M.takeP Nothing 12
+    totalCredit <- M.takeP Nothing 12
+    reserved <- M.takeP Nothing 39
+    pure AchFileControlRecord{..}
 
 instance ToAch AchFileControlRecord where
   toAch :: AchFileControlRecord -> ByteString
-  toAch (AchFileControlRecord bytes) = Char8.cons '9' bytes
+  toAch x =
+    mconcat
+      [ x.recordTypeCode
+      , x.batchCount
+      , x.blockCount
+      , x.entryAndAddendaCount
+      , x.entryHash
+      , x.totalDebit
+      , x.totalCredit
+      , x.reserved
+      ]
