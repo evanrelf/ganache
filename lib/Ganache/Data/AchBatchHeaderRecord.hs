3e45dc94b3447a7f738c3444ba068dc6c19d7549
Evan Relf <evan@evanrelf.com>
2024-06-06 21:50:08 -0700

    Break `AchBatchHeaderRecord` into opaque fields

--- lib/Ganache/Data/AchBatchHeaderRecord.hs
+++ lib/Ganache/Data/AchBatchHeaderRecord.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Ganache.Data.AchBatchHeaderRecord
@@ -8,21 +9,62 @@ where
 import Data.ByteString (ByteString)
 import Data.ByteString qualified as ByteString
 import Data.ByteString.Internal (c2w)
+import Data.Word (Word8)
 import Ganache.Class.FromAch
 import Ganache.Class.ToAch
 import Text.Megaparsec qualified as M
 import Text.Megaparsec.Byte qualified as M
 
-newtype AchBatchHeaderRecord = AchBatchHeaderRecord ByteString
+data AchBatchHeaderRecord = AchBatchHeaderRecord
+  { recordTypeCode :: !Word8
+  , serviceClassCode :: !ByteString
+  , companyName :: !ByteString
+  , companyDiscretionaryData :: !ByteString
+  , companyIdentification :: !ByteString
+  , standardEntryClassCode :: !ByteString
+  , companyEntryDescription :: !ByteString
+  , companyDescriptiveDate :: !ByteString
+  , effectiveEntryDate :: !ByteString
+  , settlementDate :: !ByteString
+  , originatorStatusCode :: !ByteString
+  , originatingDfiIdentification :: !ByteString
+  , batchNumber :: !ByteString
+  }
   deriving stock (Show, Eq)
 
 instance FromAch AchBatchHeaderRecord where
   parseAch :: Parser AchBatchHeaderRecord
   parseAch = do
-    b <- M.char (c2w '5')
-    bs <- M.takeP Nothing 93
-    pure $ AchBatchHeaderRecord (b `ByteString.cons` bs)
+    recordTypeCode <- M.char (c2w '5')
+    serviceClassCode <- M.takeP Nothing 3
+    companyName <- M.takeP Nothing 16
+    companyDiscretionaryData <- M.takeP Nothing 20
+    companyIdentification <- M.takeP Nothing 10
+    standardEntryClassCode <- M.takeP Nothing 3
+    companyEntryDescription <- M.takeP Nothing 10
+    companyDescriptiveDate <- M.takeP Nothing 6
+    effectiveEntryDate <- M.takeP Nothing 6
+    settlementDate <- M.takeP Nothing 3
+    originatorStatusCode <- M.takeP Nothing 1
+    originatingDfiIdentification <- M.takeP Nothing 8
+    batchNumber <- M.takeP Nothing 7
+    pure AchBatchHeaderRecord{..}
 
 instance ToAch AchBatchHeaderRecord where
   toAch :: AchBatchHeaderRecord -> ByteString
-  toAch (AchBatchHeaderRecord bytes) = bytes
+  toAch x =
+    mconcat
+      [ ByteString.singleton x.recordTypeCode
+      , x.serviceClassCode
+      , x.companyName
+      , x.companyDiscretionaryData
+      , x.companyIdentification
+      , x.standardEntryClassCode
+      , x.companyEntryDescription
+      , x.companyDescriptiveDate
+      , x.effectiveEntryDate
+      , x.settlementDate
+      , x.originatorStatusCode
+      , x.originatingDfiIdentification
+      , x.batchNumber
+      ]
